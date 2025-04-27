467fc6724a026122bf8e01429890389c3ee53e60
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-04-27 19:43:02 +0200

    Read config file

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Weso1ek/gator-blog-aggregator/internal/config"
+	"log"
+)
 
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("lane")
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
